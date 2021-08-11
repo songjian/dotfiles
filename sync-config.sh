47bc3550dcef1bcb623ee28cc774ada100b2e27d
宋健 <songjian@codeorder.cn>
2021-08-11 21:14:08 +0800

    添加将MyConf中配置同步到用户目录的脚本

---
+++ sync-config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+readonly BACKUP_DIR="${HOME}/configbackup/"
+
+mkdir -p "${BACKUP_DIR}"
+
+function symbolic {
+    #rsync -av "${HOME}/$1" "${BACKUP_DIR}"
+    ln -sf "${HOME}/MyConf/$1" "${HOME}/$1"
+}
+
+symbolic .profile
+symbolic .bashrc
+symbolic .bash_profile
+symbolic .gitconfig
+symbolic .viminfo
+symbolic .vimrc
+symbolic .zlogin
+symbolic .zshrc
