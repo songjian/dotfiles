0e900071473c84b2b681ce93e1ba455f26a519a6
宋健 <songjian@codeorder.cn>
2021-08-14 10:58:25 +0800

    fish添加了几个常用的别名

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
-alias open="explorer.exe"
-
 function nvm
     bass source ~/.nvm/nvm.sh ';' nvm $argv
 end
+
+alias open="explorer.exe"
+alias coin="cd ~/workspace_php/coincircle-backend/"
+alias ryf="open http://www.ruanyifeng.com/home.html"
