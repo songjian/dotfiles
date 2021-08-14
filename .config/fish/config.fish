9f8d785aa8551266c0aec784ac87459cbc089895
宋健 <songjian@codeorder.cn>
2021-08-14 11:42:36 +0800

    添加常用别名

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ end
 alias open="explorer.exe"
 alias coin="cd ~/workspace_php/coincircle-backend/"
 alias ryf="open http://www.ruanyifeng.com/home.html"
+alias jkz="open https://www.zhihu.com/people/shengjingjianke"
+alias jkx="open https://xueqiu.com/u/6784593966"
