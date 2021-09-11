b47a79a18c9f14c70c8fa033722824a9d8027f94
sj <songjian@codeorder.cn>
2021-09-11 08:56:44 +0800

    更新fish配置

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,26 @@ function nvm
     bass source ~/.nvm/nvm.sh ';' nvm $argv
 end
 
-alias open="explorer.exe"
-alias coin="cd ~/workspace_php/coincircle-backend/"
-alias ryf="open http://www.ruanyifeng.com/home.html"
-alias jkz="open https://www.zhihu.com/people/shengjingjianke"
-alias jkx="open https://xueqiu.com/u/6784593966"
-alias dsc="cd ~/workspace_php/dscmall/"
+function open
+    explorer.exe $argv
+end
+
+function coin
+    cd ~/workspace_php/coincircle-backend
+end
+
+function dsc
+    cd ~/workspace_php/dscmall
+end
+
+function ryf
+    open http://www.ruanyifeng.com/home.html
+end
+
+function jkz
+    open https://www.zhihu.com/people/shengjingjianke
+end
+
+function jkx
+    open https://xueqiu.com/u/6784593966
+end
